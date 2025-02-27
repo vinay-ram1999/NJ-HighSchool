83abe0d806c86ae7c2ea8c847e8b53449603427d
Vinay Ram Gazula <vinayramgazula@gmail.com>
2025-02-27 02:01:50 -0500

    added nj doe data for 6 years; implemented file imports

---
+++ settings.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
+nj_doe_data_dir = os.path.abspath("./nj_doe/")
--- src/nj_doe_etl.py
+++ src/nj_doe_etl.py
@@ -1,3 +1,14 @@
 import polars as pl
 
+import os
+
+from settings import nj_doe_data_dir
+
+data_dir = {dir: f"{nj_doe_data_dir}/{dir}" for dir in os.listdir(nj_doe_data_dir) if os.path.isdir(f"{nj_doe_data_dir}/{dir}")}
+data_dir = dict(sorted(data_dir.items()))
+
+for key, value in data_dir.items():
+    file_path = [f"{value}/{file}" for file in os.listdir(value) if file.split(".")[-1] == "xlsx"][0]
+    frames = pl.read_excel(file_path)
+    print(frames)
 
