a8f68d0d7cfb3580f29047df85f10b0db1f449b0
Vinay Ram Gazula <vinayramgazula@gmail.com>
2025-03-03 04:19:40 -0500

    extracted data from all academic years and merged; should fix the data types;

--- settings.py
+++ settings.py
@@ -7,10 +7,15 @@ load_dotenv()
 nj_doe_data_dir = os.path.abspath("./nj_doe/")
 
 nj_doe_required_sheets = {
-                            "Header and Contact": ["COUNTY_CODE", "DISTRICT_CODE", "SCHOOL_CODE", "COUNTY_NAME", "DISTRICT_NAME", "SCHOOL_NAME", "ADDRESS", "CITY_STATE_ZIP", "WEBSITE"], 
-                            "EnrollmentTrendsbyGrade": ["CountyCode", "DistrictCode", "SchoolCode", "Grade06", "Grade07", "Grade08", "Grade09", "Grade10", "Grade11", "Grade12"], 
-                            "EnrollmentTrendsByStudentGroup": ["CountyCode", "DistrictCode", "SchoolCode", "Economically Disadvantaged Students", "English Learners", "Female", "Homeless Students", "Male", "Migrant Students", "Military-Connected Students", "Students in Foster Care", "Students with Disabilities"], 
-                            "EnrollmentByRacialEthnicGroup": ["CountyCode", "DistrictCode", "SchoolCode", "American Indian or Alaska Native", "Asian", "Black or African American", "Hispanic", "Native Hawaiian or Pacific Islander", "Two or More Races", "White"], 
-                            "EnrollmentByHomeLanguage": ["CountyCode", "DistrictCode", "SchoolCode", "HomeLanguage", "%OfStudents"]
+                            "Header and Contact": {"input": ["COUNTY_CODE", "DISTRICT_CODE", "SCHOOL_CODE", "COUNTY_NAME", "DISTRICT_NAME", "SCHOOL_NAME", "ADDRESS", "CITY_STATE_ZIP"],
+                                                   "output": ["county_code", "district_code", "school_code", "county_name", "district_name", "school_name", "address", "city_state_zip"]}, 
+                            "EnrollmentTrendsbyGrade": {"input": ["CountyCode", "DistrictCode", "SchoolCode", "Grade06", "Grade07", "Grade08", "Grade09", "Grade10", "Grade11", "Grade12"],
+                                                        "output": ["county_code", "district_code", "school_code", "grade06", "grade07", "grade08", "grade09", "grade10", "grade11", "grade12"]}, 
+                            "EnrollmentTrendsByStudentGroup": {"input": ["CountyCode", "DistrictCode", "SchoolCode", "Economically Disadvantaged Students", "English Learners", "Female", "Homeless Students", "Male", "Migrant Students", "Military-Connected Students", "Students in Foster Care", "Students with Disabilities"],
+                                                               "output": ["county_code", "district_code", "school_code", "economically_disadvantaged", "english_learners", "female", "homeless", "male", "migrant", "military_connected", "in_foster_care", "with_disabilities"]}, 
+                            "EnrollmentByRacialEthnicGroup": {"input": ["CountyCode", "DistrictCode", "SchoolCode", "American Indian or Alaska Native", "Asian", "Black or African American", "Hispanic", "Native Hawaiian or Pacific Islander", "Two or More Races", "White"],
+                                                              "output": ["county_code", "district_code", "school_code", "american_indian_or_alaska_native", "asian", "black_or_african_american", "hispanic", "native_hawaiian_or_pacific_islander", "two_or_more_races", "white"]}, 
+                            "EnrollmentByHomeLanguage": {"input": ["CountyCode", "DistrictCode", "SchoolCode", "HomeLanguage", "%OfStudents"],
+                                                         "output": ["county_code", "district_code", "school_code", "home_language", "pct_of_students"]}
 }
 
--- src/nj_doe_etl.py
+++ src/nj_doe_etl.py
@@ -1,27 +1,39 @@
+from polars import col
 import polars as pl
-import pandas as pd
 
 import logging
 import os
 
-from settings import nj_doe_data_dir
+from settings import nj_doe_data_dir, nj_doe_required_sheets
 
 logging.getLogger()
 
 data_dir = {dir: f"{nj_doe_data_dir}/{dir}" for dir in os.listdir(nj_doe_data_dir) if os.path.isdir(f"{nj_doe_data_dir}/{dir}")}
 data_dir = dict(sorted(data_dir.items()))
 
-for key, value in data_dir.items():
-    year_sheet_cols = {'year':[], 'sheet':[], 'columns':[], 'n_columns':[]}
-    file_path = [f"{value}/{file}" for file in os.listdir(value) if file.split(".")[-1] == "xlsx"][0]
-    frames = pl.read_excel(file_path, sheet_id=0)
-    for name, sheet in frames.items():
-        if not "Important " in name:
-            cols = sheet.columns
-            year_sheet_cols['year'] += [f"{key}-{int(key)+1}"]
-            year_sheet_cols['sheet'] += [name]
-            year_sheet_cols['columns'] += [", ".join(sorted(cols))]
-            year_sheet_cols['n_columns'] += [len(cols)]
-    df = pd.DataFrame(year_sheet_cols)
-    df.to_csv(f"{nj_doe_data_dir}/{key}-{int(key)+1}_metadata.csv")
+year_merged_lfs: list[pl.LazyFrame] = []
 
+for key, value in data_dir.items():
+    logging.info(f"------- extracting {key}-{int(key)+1} academic year data -------")
+    year_lfs: list[pl.LazyFrame] = []
+    file_path = [f"{value}/{file}" for file in os.listdir(value) if file.split(".")[-1] == "xlsx"][0]
+    for sheet, schema in nj_doe_required_sheets.items():
+        logging.info(f"------- extracting sheet '{sheet}' -------")
+        assert len(schema["input"]) == len(schema["output"]), f"Miss match in number of columns in sheet '{sheet}' input/output schemas"
+        schema_map = {i:o for i,o in zip(schema["input"], schema["output"])}
+        lf = pl.read_excel(file_path, sheet_name=sheet, columns=schema["input"], infer_schema_length=0).lazy()
+        lf = lf.rename(schema_map)
+        lf = lf.with_columns(pl.concat_str([col("county_code"), col("district_code"), col("school_code")], separator="-")
+                                .alias("county_district_school_code")
+                            ).drop("county_code", "district_code", "school_code")
+        year_lfs += [lf]
+    logging.info("------- joining the sheets -------")
+    merged_lf = year_lfs[0]
+    for lf in year_lfs[1:]:
+        merged_lf = merged_lf.join(lf, on="county_district_school_code",)
+    merged_lf = merged_lf.with_columns(pl.lit(f"{key}-{int(key)+1}").alias("academic_year"))
+    year_merged_lfs += [merged_lf]
+
+logging.info("------- merging all academic years data -------")
+final_lf = pl.concat(year_merged_lfs)
+logging.info(final_lf.collect_schema().names())
