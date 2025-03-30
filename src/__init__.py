9d72777b9d4aa94338bb5d0814d9dd169b1cb77a
Vinay Ram Gazula <vinayramgazula@gmail.com>
2025-03-30 18:12:42 -0400

    added logic to load data to mysql database;

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,10 @@
+from dotenv import load_dotenv
+
 import logging
 import sys
 
 logging.basicConfig(filename=f"{__name__}.log", level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+load_dotenv()
+logging.info("------- loaded '.env' variables -------")
---
+++ src/nj_doe_load.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine
+import polars as pl
+
+import logging
+import os
+
+from settings import nj_doe_data_dir
+
+logging.getLogger()
+
+MYSQL_USER = os.getenv("MYSQL_USER", "")
+MYSQL_PWD = os.getenv("MYSQL_PWD", "")
+MYSQL_HOST = os.getenv("MYSQL_HOST", "")
+MYSQL_PORT = os.getenv("MYSQL_PORT", "")
+MYSQL_DB_NAME = os.getenv("MYSQL_DB_NAME", "")
+MYSQL_TABLE_NAME = os.getenv("MYSQL_TABLE_NAME", "")
+
+mysql_uri = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PWD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB_NAME}"
+
+try:
+    mysql_engine = create_engine(mysql_uri)
+
+    df = pl.read_csv(f"{nj_doe_data_dir}/merged_data_export.csv", infer_schema_length=None)
+    logging.info("------- writing data to MySQL database -------")
+    
+    nrows = df.write_database(MYSQL_TABLE_NAME, connection=mysql_engine, if_table_exists='replace')
+    assert nrows == df.shape[0], f"Only {nrows}/{df.shape[0]} rows are inserted"
+    
+    logging.info(f"{nrows} rows inserted into {MYSQL_DB_NAME}.{MYSQL_TABLE_NAME} table")
+except Exception as e:
+    logging.error(e)
+    raise(e)
+
