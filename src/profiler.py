97a7ad7c93b446af815d93939e508db1fc274b66
Vinay Ram Gazula <vinayramgazula@gmail.com>
2025-04-14 21:39:24 -0400

    generated profile heatmap for both fact tables;

--- src/profiler.py
+++ src/profiler.py
@@ -4,44 +4,47 @@ import polars as pl
 
 from settings import nj_doe_data_dir
 
-df = pl.read_csv(f"{nj_doe_data_dir}/fct_enrollments.csv")
+tables = ["fct_enrollments", "fct_homelang_enrollments"]
 
-attributes = [col for col in df.columns if col != "academic_year"]
+for table in tables:
+    df = pl.read_csv(f"{nj_doe_data_dir}/{table}.csv")
 
-df_unpivot = df.unpivot(
-    index="academic_year", 
-    on=attributes, 
-    variable_name="variable", 
-    value_name="value"
-)
+    attributes = [col for col in df.columns if col != "academic_year"]
 
-df_grouped = df_unpivot.with_columns(pl.col("value").is_null().alias("is_null"))\
-    .group_by(["academic_year", "variable"])\
-    .agg([
-        pl.count("value").alias("total"),
-        pl.sum("is_null").alias("missing")
-    ])\
-    .with_columns((((pl.col("total") - pl.col("missing")) / pl.col("total")) * 100).alias("fullness"))
+    df_unpivot = df.unpivot(
+        index="academic_year", 
+        on=attributes, 
+        variable_name="variable", 
+        value_name="value"
+    )
 
-heatmap_df = df_grouped.pivot(
-    values="fullness",
-    index="variable",
-    on="academic_year"
-)
+    df_grouped = df_unpivot.with_columns(pl.col("value").is_null().alias("is_null"))\
+        .group_by(["academic_year", "variable"])\
+        .agg([
+            pl.count("value").alias("total"),
+            pl.sum("is_null").alias("missing")
+        ])\
+        .with_columns((((pl.col("total") - pl.col("missing")) / pl.col("total")) * 100).alias("fullness"))
 
-heatmap_pd = heatmap_df.to_pandas()
-heatmap_pd = heatmap_pd.set_index("variable")
-heatmap_pd = heatmap_pd.reindex(attributes)
+    heatmap_df = df_grouped.pivot(
+        values="fullness",
+        index="variable",
+        on="academic_year"
+    )
 
-year_order = sorted(heatmap_pd.columns)
-heatmap_pd = heatmap_pd[year_order]
+    heatmap_pd = heatmap_df.to_pandas()
+    heatmap_pd = heatmap_pd.set_index("variable")
+    heatmap_pd = heatmap_pd.reindex(attributes)
 
-plt.figure(figsize=(10, 8))
-ax = sns.heatmap(heatmap_pd, annot=True, cmap="YlGnBu", fmt=".1f")
-plt.title("Percentage Fullness Heatmap for 'fct_enrollments'")
-plt.xlabel("Academic Year")
-plt.ylabel("Attribute")
+    year_order = sorted(heatmap_pd.columns)
+    heatmap_pd = heatmap_pd[year_order]
 
-plt.tight_layout()
-plt.savefig(f"{nj_doe_data_dir}/profile_heatmap.png", dpi=300, bbox_inches='tight')
-plt.show()
+    plt.figure(figsize=(10, 8))
+    ax = sns.heatmap(heatmap_pd, annot=True, cmap="YlGnBu", fmt=".1f")
+    plt.title(f"Percentage Fullness Heatmap for '{table}'")
+    plt.xlabel("Academic Year")
+    plt.ylabel("Attribute")
+
+    plt.tight_layout()
+    plt.savefig(f"{nj_doe_data_dir}/{table}_heatmap.png", dpi=300, bbox_inches='tight')
+    plt.show()
